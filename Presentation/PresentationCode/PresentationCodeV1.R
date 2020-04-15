f20a55571969005cb888cebb7f47c78926a48bd8
Lee Panter <lee.panter@yahoo.com>
2020-04-15 02:02:46 -0600

    Initial Code Doc Push

---
+++ Presentation/PresentationCode/PresentationCodeV1.R
@@ -0,0 +1,63 @@
+####	Blank R Script	 ####
+####	Script Name: Blank_Script.R
+
+#-------------------------------------------------------------------------#
+####	Description:	 ####
+#
+
+
+####	Script Dependencies	 ####
+
+# Package Dependencies:
+
+
+# Set Working Directory
+# WD="____" # <-- Replace ____ with working directory
+# setwd(WD)
+
+# Data Dependencies:
+
+# Variable Dependencies:
+
+# File Dependencies
+
+# url.data="_____" # <-- Replace ____ with raw.github.com -- .csv.gz datafile
+# url.source="____"  # <-- Replace ____ with raw.github.com -- .R Script File
+
+# dat.csv.gz="LocalDatFile.csv.gz"
+# models.R="LocalSourceFile.R"
+
+# download.file(url.data, LocalDatFile.csv.gz)
+# dat=read.csv(gzfile("LocalDatFile.csv.gz"))
+
+# source_url(url.source, sha_url(url.source))
+
+
+#-------------------------------------------------------------------------#
+####	Begin Script	 ####
+#-------------------------------------------------------------------------#
+
+
+
+
+
+#-------------------------------------------------------------------------#
+####	End Script	 ####
+#-------------------------------------------------------------------------#
+
+
+
+
+
+#-------------------------------------------------------------------------#
+#####	Post-Script	#####
+
+####  Notes:
+
+####  Compilation Errors:
+
+####  Execution Errors:
+
+####  Next Scripts to Consider:
+
+#-------------------------------------------------------------------------#
